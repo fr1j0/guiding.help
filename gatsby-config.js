e6ffb8498634542fcc37c65996218b48cbd156ac
fr1j0 <fercasti@gmail.com>
2021-03-16 23:19:34 +0400

    1st commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Guided Help`,
+    description: `Online counseling services.`,
+    author: `@fr1j0`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
