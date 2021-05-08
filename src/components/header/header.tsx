26d2a1b71201d9f7c1d6fbeb9a8584bee96d34d3
fr1j0 <fercasti@gmail.com>
2021-05-08 16:31:30 +0400

    cleanup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,56 +10,54 @@ type Props = {
 
 const Header = ({ menuEnabled, scrolled }: Props) => (
   <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
-    <div className="header__content">
-      <Link to="/">
-        <StaticImage
-          className={`logo ${scrolled ? "logo--scrolled" : ""}`}
-          src="../../images/guiding-help-logo.svg"
-          quality={100}
-          alt="Guiding help"
-          layout="fullWidth"
-          placeholder="none"
-        />
-      </Link>
-      <input
-        type="checkbox"
-        id="nav-toggle"
-        className="header__nav-toggle"
-        onChange={() => {}}
-        {...(menuEnabled ? {} : { checked: false })}
+    <Link to="/">
+      <StaticImage
+        className={`logo ${scrolled ? "logo--scrolled" : ""}`}
+        src="../../images/guiding-help-logo.svg"
+        quality={100}
+        alt="Guiding help"
+        layout="fullWidth"
+        placeholder="none"
       />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" activeClassName="active">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/my-story/" activeClassName="active">
-              My story
-            </Link>
-          </li>
-          <li>
-            <Link to="/services/" activeClassName="active">
-              Services
-            </Link>
-          </li>
-          <li>
-            <a
-              className="btn-border"
-              href="https://calendly.com/joanne-141/free-introductory-session"
-              target="blank"
-            >
-              Book a call
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <label htmlFor="nav-toggle" className="nav-toggle-label">
-        <span></span>
-      </label>
-    </div>
+    </Link>
+    <input
+      type="checkbox"
+      id="nav-toggle"
+      className="header__nav-toggle"
+      onChange={() => {}}
+      {...(menuEnabled ? {} : { checked: false })}
+    />
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" activeClassName="active">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/my-story/" activeClassName="active">
+            My story
+          </Link>
+        </li>
+        <li>
+          <Link to="/services/" activeClassName="active">
+            Services
+          </Link>
+        </li>
+        <li>
+          <a
+            className="btn-border"
+            href="https://calendly.com/joanne-141/free-introductory-session"
+            target="blank"
+          >
+            Book a call
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <label htmlFor="nav-toggle" className="nav-toggle-label">
+      <span></span>
+    </label>
   </header>
 )
 
