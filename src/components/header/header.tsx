eaeaf47ba0c3a81690411f0e1e61fc5631df727c
fr1j0 <fercasti@gmail.com>
2021-05-08 17:51:39 +0400

    cleanup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject } from "react"
+import React, { RefObject, useState } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import "./styles.sass"
@@ -8,57 +8,69 @@ type Props = {
   scrolled: boolean
 }
 
-const Header = ({ menuEnabled, scrolled }: Props) => (
-  <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
-    <Link to="/">
-      <StaticImage
-        className={`logo ${scrolled ? "logo--scrolled" : ""}`}
-        src="../../images/guiding-help-logo.svg"
-        quality={100}
-        alt="Guiding help"
-        layout="fullWidth"
-        placeholder="none"
+const Header = ({ menuEnabled, scrolled }: Props) => {
+  const [checked, setChecked] = useState(false)
+
+  return (
+    <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
+      <Link to="/">
+        <StaticImage
+          className={`logo ${scrolled ? "logo--scrolled" : ""}`}
+          src="../../images/guiding-help-logo.svg"
+          quality={100}
+          alt="Guiding help"
+          layout="fullWidth"
+          placeholder="none"
+        />
+      </Link>
+      <input
+        type="checkbox"
+        id="nav-toggle"
+        className="header__nav-toggle"
+        onChange={e => {
+          setChecked(e.target.checked)
+        }}
+        {...(menuEnabled ? {} : { checked: false })}
       />
-    </Link>
-    <input
-      type="checkbox"
-      id="nav-toggle"
-      className="header__nav-toggle"
-      onChange={() => {}}
-      {...(menuEnabled ? {} : { checked: false })}
-    />
-    <nav>
-      <ul>
-        <li>
-          <Link to="/" activeClassName="active">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/my-story/" activeClassName="active">
-            My story
-          </Link>
-        </li>
-        <li>
-          <Link to="/services/" activeClassName="active">
-            Services
-          </Link>
-        </li>
-        <li>
-          <a
-            className="btn-border"
-            href="https://calendly.com/joanne-141/free-introductory-session"
-            target="blank"
-          >
-            Book a call
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <label htmlFor="nav-toggle" className="nav-toggle-label">
-      <span></span>
-    </label>
-  </header>
-)
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" activeClassName="active">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/my-story/" activeClassName="active">
+              My story
+            </Link>
+          </li>
+          <li>
+            <Link to="/services/" activeClassName="active">
+              Services
+            </Link>
+          </li>
+          <li>
+            <a
+              className="btn-border"
+              href="https://calendly.com/joanne-141/free-introductory-session"
+              target="blank"
+            >
+              Book a call
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {checked ? (
+        <label htmlFor="nav-toggle" className="nav-toggle-label">
+          &#x2715;
+        </label>
+      ) : (
+        <label htmlFor="nav-toggle" className="nav-toggle-label">
+          &#x2630;
+        </label>
+      )}
+    </header>
+  )
+}
 
 export default Header
