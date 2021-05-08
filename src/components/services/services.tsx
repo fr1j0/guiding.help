bf4e26ce9ba9a5faf8a75365081349f74c8f05bf
fr1j0 <fercasti@gmail.com>
2021-05-08 22:13:08 +0400

    cleanup

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -5,63 +5,55 @@ import "./styles.sass"
 const Services = () => {
   return (
     <section className="services">
-      {/* <StaticImage
-        className="services-image"
-        src="../../images/jo2.jpg"
-        width={250}
-        height={400}
-        quality={100}
-        alt="Guiding help"
-        layout="fixed"
-        placeholder="none"
-      /> */}
-      <h1>Services</h1>
-      <h2 id="general-counselling">General Counselling</h2>
-      <p>
-        The counselling process is a planned, structured dialogue between a
-        counsellor and a client. It is a cooperative process in which a trained
-        professional helps a person called the client to identify sources of
-        difficulties or concerns that he or she is experiencing.
-      </p>
-      <h2 id="sexual-abuse">Sexual Abuse</h2>
-      <p>
-        Counsellors who help survivors are trained to help with the
-        psychological effects of sexual violence. This may include low
-        confidence levels, anxiety, depression or post-traumatic stress disorder
-        (PTSD). You can speak to a counsellor at any time - even if you
-        experienced sexual abuse many years ago.
-      </p>
-      <h2 id="grief-and-bereavement">Grief and Bereavement</h2>
-      <p>
-        Your bereavement counsellor will help you better understand the grieving
-        process, process your grief, and assist you in working through the
-        stages of grief. Your therapist will support you to work through
-        difficult feelings around your loss. How does bereavement counselling
-        work? Typically a counsellor or psychologist will encourage the grieving
-        person to share their thoughts and feelings about the loss, and
-        encourage them to engage with life in a way which helps them to recover
-        from their grief.
-      </p>
-      <h2 id="children-and-adolescents">Children and Adolescents</h2>
-      <p>
-        Child counseling is a type of therapy that focuses on young children,
-        teens, and adolescents with one or more mental illnesses. It also
-        provides aid to youths, who have experienced trauma, and/or who are
-        experiencing a dysfunctional or stressful home environment
-      </p>
-      <h2 id="depression">Depression</h2>
-      <p>
-        Depression (major depressive disorder) is a common and serious medical
-        illness that negatively affects how you feel, the way you think and how
-        you act. Fortunately, it is also treatable. Depression causes feelings
-        of sadness and/or a loss of interest in activities you once enjoyed. It
-        can lead to a variety of emotional and physical problems and can
-        decrease your ability to function at work and at home. If there is no
-        underlying medical cause for your symptoms of depression, talk therapy
-        (counselling/coaching) can be an extremely effective treatment. What you
-        learn in therapy gives you skills and insight to feel better and help
-        prevent depression from coming back
-      </p>
+      <div className="services__text">
+        <h1>Services</h1>
+        <h2 id="general-counselling">General Counselling</h2>
+        <p>
+          The counselling process is a planned, structured dialogue between a
+          counsellor and a client. It is a cooperative process in which a
+          trained professional helps a person called the client to identify
+          sources of difficulties or concerns that he or she is experiencing.
+        </p>
+        <h2 id="sexual-abuse">Sexual Abuse</h2>
+        <p>
+          Counsellors who help survivors are trained to help with the
+          psychological effects of sexual violence. This may include low
+          confidence levels, anxiety, depression or post-traumatic stress
+          disorder (PTSD). You can speak to a counsellor at any time - even if
+          you experienced sexual abuse many years ago.
+        </p>
+        <h2 id="grief-and-bereavement">Grief and Bereavement</h2>
+        <p>
+          Your bereavement counsellor will help you better understand the
+          grieving process, process your grief, and assist you in working
+          through the stages of grief. Your therapist will support you to work
+          through difficult feelings around your loss. How does bereavement
+          counselling work? Typically a counsellor or psychologist will
+          encourage the grieving person to share their thoughts and feelings
+          about the loss, and encourage them to engage with life in a way which
+          helps them to recover from their grief.
+        </p>
+        <h2 id="children-and-adolescents">Children and Adolescents</h2>
+        <p>
+          Child counseling is a type of therapy that focuses on young children,
+          teens, and adolescents with one or more mental illnesses. It also
+          provides aid to youths, who have experienced trauma, and/or who are
+          experiencing a dysfunctional or stressful home environment
+        </p>
+        <h2 id="depression">Depression</h2>
+        <p>
+          Depression (major depressive disorder) is a common and serious medical
+          illness that negatively affects how you feel, the way you think and
+          how you act. Fortunately, it is also treatable. Depression causes
+          feelings of sadness and/or a loss of interest in activities you once
+          enjoyed. It can lead to a variety of emotional and physical problems
+          and can decrease your ability to function at work and at home. If
+          there is no underlying medical cause for your symptoms of depression,
+          talk therapy (counselling/coaching) can be an extremely effective
+          treatment. What you learn in therapy gives you skills and insight to
+          feel better and help prevent depression from coming back
+        </p>
+      </div>
     </section>
   )
 }
