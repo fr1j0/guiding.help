ac3f44c7f6647343d2ebd540e898f918200932e1
fr1j0 <fercasti@gmail.com>
2021-05-08 21:32:27 +0400

    cleanup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -13,62 +13,64 @@ const Header = ({ menuEnabled, scrolled }: Props) => {
 
   return (
     <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
-      <Link to="/">
-        <StaticImage
-          className={`logo ${scrolled ? "logo--scrolled" : ""}`}
-          src="../../images/guiding-help-logo.svg"
-          quality={100}
-          alt="Guiding help"
-          layout="fullWidth"
-          placeholder="none"
+      <div className="header__content">
+        <Link to="/">
+          <StaticImage
+            className={`logo ${scrolled ? "logo--scrolled" : ""}`}
+            src="../../images/guiding-help-logo.svg"
+            quality={100}
+            alt="Guiding help"
+            layout="fullWidth"
+            placeholder="none"
+          />
+        </Link>
+        <input
+          type="checkbox"
+          id="nav-toggle"
+          className="header__nav-toggle"
+          onChange={e => {
+            setChecked(e.target.checked)
+          }}
+          {...(menuEnabled ? {} : { checked: false })}
         />
-      </Link>
-      <input
-        type="checkbox"
-        id="nav-toggle"
-        className="header__nav-toggle"
-        onChange={e => {
-          setChecked(e.target.checked)
-        }}
-        {...(menuEnabled ? {} : { checked: false })}
-      />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" activeClassName="active">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/my-story/" activeClassName="active">
-              My story
-            </Link>
-          </li>
-          <li>
-            <Link to="/services/" activeClassName="active">
-              Services
-            </Link>
-          </li>
-          <li>
-            <a
-              className="btn-border"
-              href="https://calendly.com/joanne-141/free-introductory-session"
-              target="blank"
-            >
-              Book a call
-            </a>
-          </li>
-        </ul>
-      </nav>
-      {checked ? (
-        <label htmlFor="nav-toggle" className="nav-toggle-label">
-          &#x2715;
-        </label>
-      ) : (
-        <label htmlFor="nav-toggle" className="nav-toggle-label">
-          &#x2630;
-        </label>
-      )}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/my-story/" activeClassName="active">
+                My story
+              </Link>
+            </li>
+            <li>
+              <Link to="/services/" activeClassName="active">
+                Services
+              </Link>
+            </li>
+            <li>
+              <a
+                className="btn-border"
+                href="https://calendly.com/joanne-141/free-introductory-session"
+                target="blank"
+              >
+                Book a call
+              </a>
+            </li>
+          </ul>
+        </nav>
+        {checked ? (
+          <label htmlFor="nav-toggle" className="nav-toggle-label">
+            &#x2715;
+          </label>
+        ) : (
+          <label htmlFor="nav-toggle" className="nav-toggle-label">
+            &#x2630;
+          </label>
+        )}
+      </div>
     </header>
   )
 }
